e93f3b17de592166e250124471cff13abc48258b
Arjun Ajith <arjuunajith@gmail.com>
2020-06-17 22:52:57 +0530

    visualization

--- Main.R
+++ Main.R
@@ -1,4 +1,16 @@
 # attach the iris dataset to the environment
 data(iris)
-# rename the dataset
 dataset <- iris
+
+validation_index <- createDataPartition(dataset$Species, p=0.80, list=FALSE)
+validation <- dataset[-validation_index,]
+dataset <- dataset[validation_index,]
+
+percentage <- prop.table(table(dataset$Species)) * 100
+cbind(freq=table(dataset$Species), percentage=percentage)
+
+x <- dataset[,1:4]
+y <- dataset[,5]
+par(mfrow=c(1,4))
+for(i in 1:4) {boxplot(x[,i], main=names(iris)[i])}
+
